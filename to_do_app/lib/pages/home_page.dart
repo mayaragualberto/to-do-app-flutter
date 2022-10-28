aa8f0db01ea31a5cae33d56da378779387a93951
Mayara Gualberto <mayaragualberto@yahoo.com.br>
2022-10-28 16:06:22 -0300

    first commit

---
+++ to_do_app/lib/pages/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["Make tutorial", false],
+    ["Do Exercise", false],
+    ["Make tutorial", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: const Center(child: Text("TO DO")),
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
